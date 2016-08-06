7b3103287395129a249a9beeaa7005d90d2f06f3
Jonas Wiklund <jwiklund@gmail.com>
2016-08-06 19:55:15 +0200

    add shortcuts

--- main.go
+++ main.go
@@ -16,9 +16,13 @@ var usage = `Todo list.
 
 Usage:
   todo -h
+  todo [-av][-r <repo>]
   todo [-av][-r <repo>] list
   todo [-v][-r <repo>] add <message>...
   todo [-v][-r <repo>] update <id> <state>
+  todo [-v][-r <repo>] do <id>
+  todo [-v][-r <repo>] wait <id>
+  todo [-v][-r <repo>] done <id>
     
 Options:
   -a          include all tasks [default false]
@@ -75,23 +79,6 @@ func repo(opts map[string]interface{}) todo.Repo {
 }
 
 func cmd(r todo.Repo, opts map[string]interface{}) {
-	if opts["list"].(bool) {
-		all := opts["-a"].(bool)
-		tasks, err := r.List()
-		if err != nil {
-			log.Error("Couldn't list tasks ", err.Error())
-			log.Debugf("%+v", err)
-			return
-		}
-		w := tabwriter.NewWriter(os.Stdout, 6, 8, 1, ' ', 0)
-		for _, task := range tasks {
-			if all || task.IsCurrent() {
-				w.Write([]byte(task.String()))
-				w.Write([]byte{'\n'})
-			}
-		}
-		w.Flush()
-	}
 	if opts["add"].(bool) {
 		messages := opts["<message>"].([]string)
 		message := strings.Join(messages, " ")
@@ -102,25 +89,57 @@ func cmd(r todo.Repo, opts map[string]interface{}) {
 			return
 		}
 		fmt.Println("Created ", task)
+		return
 	}
 	if opts["update"].(bool) {
-		id := opts["<id>"].(string)
-		if !todo.StateValid(opts["<state>"].(string)) {
-			log.Debug("Invalid state ", opts["<state>"].(string))
+		update(r, opts["<id>"].(string), opts["<state>"].(string))
+		return
+	}
+	if opts["do"].(bool) {
+		update(r, opts["<id>"].(string), "doing")
+		return
+	}
+	if opts["wait"].(bool) {
+		update(r, opts["<id>"].(string), "waiting")
+		return
+	}
+	if opts["done"].(bool) {
+		update(r, opts["<id>"].(string), "done")
+		return
+	}
+	all := opts["-a"].(bool)
+	tasks, err := r.List()
+	if err != nil {
+		log.Error("Couldn't list tasks ", err.Error())
+		log.Debugf("%+v", err)
+		return
+	}
+	w := tabwriter.NewWriter(os.Stdout, 6, 8, 1, ' ', 0)
+	for _, task := range tasks {
+		if all || task.IsCurrent() {
+			w.Write([]byte(task.String()))
+			w.Write([]byte{'\n'})
 		}
-		state := todo.StateFrom(opts["<state>"].(string))
-		task, err := r.Get(id)
-		if err != nil {
-			log.Error("Could not find task ", err.Error())
-			log.Debugf("%+v", err)
-			return
-		}
-		task.State = state
-		err = r.Update(task)
-		if err != nil {
-			log.Error("Could not update task ", err.Error())
-			log.Debugf("%+v", err)
-			return
-		}
+	}
+	w.Flush()
+}
+
+func update(r todo.Repo, id, state string) {
+	if !todo.StateValid(state) {
+		log.Debug("Invalid state ", state)
+	}
+	s := todo.StateFrom(state)
+	task, err := r.Get(id)
+	if err != nil {
+		log.Error("Could not find task ", err.Error())
+		log.Debugf("%+v", err)
+		return
+	}
+	task.State = s
+	err = r.Update(task)
+	if err != nil {
+		log.Error("Could not update task ", err.Error())
+		log.Debugf("%+v", err)
+		return
 	}
 }
