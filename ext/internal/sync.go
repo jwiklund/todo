d4f180f9c35dab5586502383479fdbbb2d67e48b
Jonas Wiklund <jwiklund@gmail.com>
2016-08-15 19:40:12 +0200

    extract sync logic

---
+++ ext/internal/sync.go
@@ -0,0 +1,135 @@
+package internal
+
+import (
+	"strconv"
+
+	"github.com/deckarep/golang-set"
+	"github.com/jwiklund/todo/todo"
+)
+
+type indexedTasks struct {
+	tasks []todo.Task
+	index map[string]int
+}
+
+// SyncHelper common sync implememntation
+func SyncHelper(r todo.RepoBegin, extID string, externalCurrent, localCurrent []todo.Task) error {
+	external := index(extID, externalCurrent)
+	local := index(extID, localCurrent)
+
+	externalIds := external.IDSet()
+	localIds := local.IDSet()
+
+	added := externalIds.Difference(localIds)
+	missing := localIds.Difference(externalIds)
+	updated := updated(external, local)
+
+	commitRepo, err := r.Begin()
+	if err != nil {
+		return err
+	}
+	if err := syncAdd(commitRepo, extID, external, added); err != nil {
+		commitRepo.Close()
+		return err
+	}
+	if err := syncRemove(commitRepo, local, missing); err != nil {
+		commitRepo.Close()
+		return err
+	}
+	if err := syncUpdate(commitRepo, external, local, updated); err != nil {
+		commitRepo.Close()
+		return err
+	}
+	return commitRepo.Commit()
+}
+
+func index(extID string, tasks []todo.Task) *indexedTasks {
+	index := map[string]int{}
+	for i, task := range tasks {
+		if a, ok := task.Attr[extID+".id"]; ok {
+			index[a] = i
+		}
+	}
+	return &indexedTasks{tasks, index}
+}
+
+func (i *indexedTasks) IDSet() mapset.Set {
+	r := mapset.NewThreadUnsafeSet()
+	for key := range i.index {
+		r.Add(key)
+	}
+	return r
+}
+
+func currentSet(mapping map[int]int) mapset.Set {
+	s := mapset.NewThreadUnsafeSet()
+	for attr := range mapping {
+		s.Add(attr)
+	}
+	return s
+}
+
+func infileSet(lines [][]byte) mapset.Set {
+	s := mapset.NewThreadUnsafeSet()
+	for index := range lines {
+		s.Add(index)
+	}
+	return s
+}
+
+func updated(external, local *indexedTasks) mapset.Set {
+	updated := mapset.NewThreadUnsafeSet()
+
+	for externalKey, externalIndex := range external.index {
+		if localIndex, ok := local.index[externalKey]; ok {
+			if external.tasks[externalIndex].Message != local.tasks[localIndex].Message {
+				updated.Add(externalKey)
+			}
+		}
+	}
+
+	return updated
+}
+
+func syncAdd(r todo.Repo, extID string, external *indexedTasks, added mapset.Set) error {
+	for _, add := range added.ToSlice() {
+		index := external.index[add.(string)]
+		addedMessage := external.tasks[index].Message
+		_, err := r.AddWithAttr(addedMessage, map[string]string{
+			"external":    extID,
+			extID + ".id": strconv.Itoa(index),
+		})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func syncRemove(r todo.Repo, local *indexedTasks, missing mapset.Set) error {
+	for _, rem := range missing.ToSlice() {
+		index := local.index[rem.(string)]
+		t := local.tasks[index]
+		t.State = todo.StateFrom("done")
+		err := r.Update(t)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func syncUpdate(r todo.Repo, external, local *indexedTasks, updated mapset.Set) error {
+	for _, up := range updated.ToSlice() {
+		externalIndex := external.index[up.(string)]
+		localIndex := local.index[up.(string)]
+		e := external.tasks[externalIndex]
+		l := local.tasks[localIndex]
+		l.Message = e.Message
+		err := r.Update(l)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
--- ext/text/sync.go
+++ ext/text/sync.go
@@ -3,127 +3,34 @@ package text
 import (
 	"strconv"
 
-	"github.com/deckarep/golang-set"
+	"github.com/jwiklund/todo/ext/internal"
 	"github.com/jwiklund/todo/todo"
 )
 
 // Sync all available lines with all available items
 // TODO find existing done tasks before creating new tasks (needs find by attribute)
 func (t *text) Sync(r todo.RepoBegin) error {
-	tasks, err := r.List()
+	localTasks, err := r.List()
 	if err != nil {
 		return err
 	}
 
-	rowToTask := rowToTask(t.id, tasks)
-	current := currentSet(rowToTask)
-	infile := infileSet(t.source)
+	externalTasks := tasksFor(t.id, t.source)
 
-	added := infile.Difference(current)
-	missing := current.Difference(infile)
-	updated := updated(t.id, t.source, rowToTask, tasks)
+	return internal.SyncHelper(r, t.id, externalTasks, localTasks)
 
-	commitRepo, err := r.Begin()
-	if err != nil {
-		return err
-	}
-	if err := syncAdd(commitRepo, t.id, t.source, added); err != nil {
-		commitRepo.Close()
-		return err
-	}
-	if err := syncRemove(commitRepo, rowToTask, tasks, missing); err != nil {
-		commitRepo.Close()
-		return err
-	}
-	if err := syncUpdate(commitRepo, t.source, tasks, rowToTask, updated); err != nil {
-		commitRepo.Close()
-		return err
-	}
-	return commitRepo.Commit()
 }
 
-func rowToTask(id string, tasks []todo.Task) map[int]int {
-	rowToTask := map[int]int{}
-	for tasknr, task := range tasks {
-		if attr, ok := task.Attr[id+".id"]; ok {
-			attrint, err := strconv.Atoi(attr)
-			if err != nil {
-				textLog.Errorf("Invalid external id for task %s", task.ID)
-			} else {
-				rowToTask[attrint] = tasknr
-			}
-		}
-	}
-	return rowToTask
-}
-
-func currentSet(mapping map[int]int) mapset.Set {
-	s := mapset.NewThreadUnsafeSet()
-	for attr := range mapping {
-		s.Add(attr)
-	}
-	return s
-}
-
-func infileSet(lines [][]byte) mapset.Set {
-	s := mapset.NewThreadUnsafeSet()
-	for index := range lines {
-		s.Add(index)
-	}
-	return s
-}
-
-func updated(id string, source [][]byte, rowToTask map[int]int, tasks []todo.Task) mapset.Set {
-	updated := mapset.NewThreadUnsafeSet()
-	for rowIndex, bytes := range source {
-		if taskIndex, ok := rowToTask[rowIndex]; ok {
-			if string(bytes) != tasks[taskIndex].Message {
-				updated.Add(rowIndex)
-			}
-		}
-	}
-
-	return updated
-}
-
-func syncAdd(r todo.Repo, id string, source [][]byte, added mapset.Set) error {
-	for index, add := range added.ToSlice() {
-		addedMessage := string(source[add.(int)])
-		_, err := r.AddWithAttr(addedMessage, map[string]string{
-			"external": id,
-			id + ".id": strconv.Itoa(index),
+func tasksFor(id string, source [][]byte) []todo.Task {
+	var res []todo.Task
+	for i, message := range source {
+		res = append(res, todo.Task{
+			Message: string(message),
+			Attr: map[string]string{
+				"external": id,
+				id + ".id": strconv.Itoa(i),
+			},
 		})
-		if err != nil {
-			return err
-		}
 	}
-	return nil
-}
-
-func syncRemove(r todo.Repo, rowToTask map[int]int, tasks []todo.Task, missing mapset.Set) error {
-	for _, rem := range missing.ToSlice() {
-		if tindex, ok := rowToTask[rem.(int)]; ok {
-			t := tasks[tindex]
-			t.State = todo.StateFrom("done")
-			err := r.Update(t)
-			if err != nil {
-				return err
-			}
-		}
-	}
-	return nil
-}
-
-func syncUpdate(r todo.Repo, source [][]byte, tasks []todo.Task, rowToTask map[int]int, updated mapset.Set) error {
-	for _, up := range updated.ToSlice() {
-		if tindex, ok := rowToTask[up.(int)]; ok {
-			t := tasks[tindex]
-			t.Message = string(source[up.(int)])
-			err := r.Update(t)
-			if err != nil {
-				return err
-			}
-		}
-	}
-	return nil
+	return res
 }
